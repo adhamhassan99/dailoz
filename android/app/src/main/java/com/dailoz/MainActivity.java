9271a1545ac25a18cd6acb347f49653966e1ecd8
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-06-28 15:55:16 +0300

    packkage adding

--- android/app/src/main/java/com/dailoz/MainActivity.java
+++ android/app/src/main/java/com/dailoz/MainActivity.java
@@ -1,4 +1,6 @@
 package com.dailoz;
+import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +16,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "dailoz";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
